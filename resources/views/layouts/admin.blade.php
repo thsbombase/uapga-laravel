0f0116d62ee7ddc08fa54fdfbde5eca11f8012b7
Tristan <tristan.bombase13@gmail.com>
2023-01-12 16:20:08 +0800

    Bug Fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -61,7 +61,7 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="{{ route('landing') }}" class="logo d-flex align-items-center">
                 <span class="d-none d-lg-block">UAPGA</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('landing') }}">
                     UAPGA
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
