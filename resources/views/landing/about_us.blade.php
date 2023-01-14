5bc39c044c3e320bd33ae5afb86a1e3c4487e44b
Tristan <tristan.bombase13@gmail.com>
2023-01-14 11:59:52 +0800

    bug fix

--- resources/views/landing/about_us.blade.php
+++ resources/views/landing/about_us.blade.php
@@ -97,21 +97,5 @@
         </div>
     </div>
 </section>
-<!-- ======= CTA Section ======= -->
-<section class="section cta-section">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
-                <h2>A lot of perks and benefits await you as you become a member for this Fiscal Year! What are you
-                    waiting for? Register now!</h2>
-            </div>
-            <div class="col-md-5 text-center text-md-end">
-                <p><a href="{{ route('login') }}" class="btn d-inline-flex align-items-center"><span>Login</span></a> <a
-                        href="{{ route('register') }}"
-                        class="btn d-inline-flex align-items-center"><span>Register</span></a>
-                </p>
-            </div>
-        </div>
-    </div>
-</section><!-- End CTA Section -->
+@include('landing.cta')
 @endsection
---
+++ resources/views/landing/cta.blade.php
@@ -0,0 +1,19 @@
+<!-- ======= CTA Section ======= -->
+<section class="section cta-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6 me-auto text-center text-md-start mb-5 mb-md-0">
+                <h5 class="text-white">A lot of perks and benefits await you as you become a member for this Fiscal
+                    Year! <br><br>
+                    What are you waiting for? <br><br>
+                    Register now!</h5>
+            </div>
+            <div class="col-md-5 text-center text-md-end">
+                <p><a href="{{ route('login') }}" class="btn d-inline-flex align-items-center"><span>Login</span></a> <a
+                        href="{{ route('register') }}"
+                        class="btn d-inline-flex align-items-center"><span>Register</span></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section><!-- End CTA Section -->
