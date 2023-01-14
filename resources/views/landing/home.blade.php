3ca9612778e94c16ff10106ff7db10bee74514d0
Tristan <tristan.bombase13@gmail.com>
2023-01-14 21:45:30 +0800

    test

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -43,7 +43,32 @@
 
 </section><!-- End Hero -->
 
-<x-landing.logos />
+<!-- ======= Clients Section ======= -->
+<section id="clients" classd="clients">
+    <div class="container" data-aos="zoom-out">
+        <div class="clients-slider swiper">
+            <div class="swiper-wrapper align-items-center">
+                @if($logos->isNotEmpty())
+                @foreach ($logos as $logo)
+
+                <div class="swiper-slide"><img src="{{ asset('images/sponsors/'. $logo->company_logo ) }}"
+                        class="img-fluid" alt="">
+                </div>
+
+
+                @endforeach
+                @else
+
+
+
+                @endif
+
+            </div>
+        </div>
+    </div>
+
+    </div>
+</section><!-- End Clients Section -->
 
 <section class="section">
     <div class="container">
