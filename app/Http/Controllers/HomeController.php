5a9c8bdb28994f458d759a97a7439056a75aa6b8
Tristan <tristan.bombase13@gmail.com>
2023-01-14 21:52:05 +0800

    add sponsor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Announcement as AnnouncementModel;
+use App\Models\Sponsor;
 
 class HomeController extends Controller
 {
