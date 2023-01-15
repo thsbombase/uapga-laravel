d80b1a0ba5f875f49838bd57e657b6125606aa85
Tristan <tristan.bombase13@gmail.com>
2023-01-15 22:07:20 +0800

    error message

--- resources/views/admin/verified.blade.php
+++ resources/views/admin/verified.blade.php
@@ -17,11 +17,15 @@
 
 <div class="card " style="width: 18rem;">
     <div class="card-body">
-        <p class="card-text mt-3"><strong>Card Number:</strong> {{ $card->year . ' ' . $card->district_code . ' ' .
-            $card->control_number }}</p>
-        <p class="card-text mt-3"><strong>Name:</strong> {{ $user->name }}</p>
-        <p class="card-text mt-3"><strong>Valid Until:</strong> {{ date('F Y', strtotime($card->valid_until)); }} </p>
+        @if ($card && $card->valid_until < now()) <div class="alert alert-danger">
+            <p>Card is already expired.</p>
     </div>
+    @endif
+    <p class="card-text mt-3"><strong>Card Number:</strong> {{ $card->year . ' ' . $card->district_code . ' ' .
+        $card->control_number }}</p>
+    <p class="card-text mt-3"><strong>Name:</strong> {{ $user->name }}</p>
+    <p class="card-text mt-3"><strong>Valid Until:</strong> {{ date('F Y', strtotime($card->valid_until)); }} </p>
+</div>
 </div>
 <a href="{{ route('scanner') }}" class="btn btn-primary">Scan Again</a>
 
