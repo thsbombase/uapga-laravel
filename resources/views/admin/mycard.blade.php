1e8bd5d49cdc98ed105b4f557b048a7bdb1f7224
Tristan <tristan.bombase13@gmail.com>
2023-01-18 12:21:13 +0800

    fix: card

--- resources/views/admin/mycard.blade.php
+++ resources/views/admin/mycard.blade.php
@@ -1,102 +1,70 @@
 @extends('layouts.admin')
 @push('styles')
 <style>
-    h1 {
-        font-size: 2.2em;
+    .scene {
+        display: inline-block;
+        /*   border: 1px solid #CCC; */
+        margin: 40px 0;
+        perspective: 600px;
     }
 
-    .flip {
+    .cardbg {
         position: relative;
+        width: 566px;
+        height: 358px;
+        cursor: pointer;
+        transform-style: preserve-3d;
+        transform-origin: center right;
+        transition: transform 1s;
     }
 
-    .flip>.front,
-    .flip>.back {
-        display: block;
-        transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
-        transition-duration: 1s;
-        transition-property: transform, opacity;
+    .cardbg.is-flipped {
+        transform: translateX(-100%) rotateY(-180deg);
     }
 
-    .flip>.front {
-        transform: rotateY(0deg);
-    }
-
-    .flip>.back {
+    .card__face {
         position: absolute;
-        opacity: 0;
-        top: 0px;
-        left: 0px;
         width: 100%;
         height: 100%;
-        transform: rotateY(-180deg);
-    }
-
-    .flip:active>.front {
-        transform: rotateY(180deg);
-    }
-
-    .flip:active>.back {
-        opacity: 1;
-        transform: rotateY(0deg);
-    }
-
-    .flip.flip-vertical>.back {
-        transform: rotateX(-180deg);
-    }
-
-    .flip.flip-vertical:active>.front {
-        transform: rotateX(180deg);
-    }
-
-    .flip.flip-vertical:active>.back {
-        transform: rotateX(0deg);
-    }
-
-    .flip {
-        position: relative;
-        display: inline-block;
-        margin-right: 2px;
-        margin-bottom: 1em;
-        width: 566px;
-    }
-
-    .flip>.front,
-    .flip>.back {
-        display: block;
         color: white;
+        text-align: center;
+        font-weight: bold;
+        font-size: 40px;
+        backface-visibility: hidden;
+    }
+
+    .card__face--front {
+        background: rgba(255, 255, 255, 0);
         width: inherit;
         background-size: contain !important;
         background-position: center !important;
         background-repeat: no-repeat !important;
-        height: 358px;
-        padding: 1em 2em;
-        background: #f9f8f800;
-        border-radius: 18px;
     }
 
-    .flip>.front p,
-    .flip>.back p {
-        font-size: 0.9125rem;
-        line-height: 160%;
-        color: #999;
+    .card__face--back {
+        background: rgba(255, 255, 255, 0);
+        width: inherit;
+        background-size: contain !important;
+        background-position: center !important;
+        background-repeat: no-repeat !important;
+        transform: rotateY(180deg);
     }
 
-    .text-shadow {
-        text-shadow: 1px 1px rgba(0, 0, 0, 0.04), 2px 2px rgba(0, 0, 0, 0.04), 3px 3px rgba(0, 0, 0, 0.04), 4px 4px rgba(0, 0, 0, 0.04), 0.125rem 0.125rem rgba(0, 0, 0, 0.04), 6px 6px rgba(0, 0, 0, 0.04), 7px 7px rgba(0, 0, 0, 0.04), 8px 8px rgba(0, 0, 0, 0.04), 9px 9px rgba(0, 0, 0, 0.04), 0.3125rem 0.3125rem rgba(0, 0, 0, 0.04), 11px 11px rgba(0, 0, 0, 0.04), 12px 12px rgba(0, 0, 0, 0.04), 13px 13px rgba(0, 0, 0, 0.04), 14px 14px rgba(0, 0, 0, 0.04), 0.625rem 0.625rem rgba(0, 0, 0, 0.04), 16px 16px rgba(0, 0, 0, 0.04), 17px 17px rgba(0, 0, 0, 0.04), 18px 18px rgba(0, 0, 0, 0.04), 19px 19px rgba(0, 0, 0, 0.04), 1.25rem 1.25rem rgba(0, 0, 0, 0.04);
-    }
-
-    /* responsive .flip>.front,
-    .flip>.back and .flip for mobile*/
-    /* @media (max-width: 767px) {
-        .flip {
-            width: 100%;
+    /* set card height and witdt for mobile */
+    @media (max-width: 767px) {
+        .cardbg {
+            position: relative;
+            margin: 0;
+            width: 100vw;
+            height: 25vh;
         }
-
-        .flip>.front,
-        .flip>.back {
-            height: 223px;
+        #qrcode img {
+            justify-content: flex-end;
+            width: 60%;
+            height: 60%;
         }
-    } */
+        
+    }
 </style>
 @endpush
 @push('scripts')
@@ -120,6 +88,13 @@
 
     makeCode();
     
+    var cards = document.querySelectorAll('.cardbg');
+
+    [...cards].forEach((card)=>{
+        card.addEventListener( 'click', function() {
+            card.classList.toggle('is-flipped');
+        });
+    });
 </script>
 @endif
 @endpush
@@ -138,12 +113,41 @@
     {!! \Session::get('success') !!}
 </div>
 @endif
-<div class="text-center">
+<div class="text-center m-0 p-0">
     @if ($card && $card->valid_until < now()) <div class="alert alert-danger">
         <p>Your card is already expired.</p>
 
         @elseif (Auth::user()->status == 'approved')
-        <div class="flip me-2">
+        <div class="scene scene--cardbg">
+            <div class="cardbg">
+                <div class="card__face card__face--front "
+                    style="background-image: url('{{ asset('admin/img/FRONT_OPTION 1.webp') }}');">
+                    <div class=" d-flex align-items-end justify-content-end h-100 w-100 p-3">
+                        <div>
+                            <p class=" text-end text-white m-0" style="font-size: 11px">{{ $card->year . '
+                                ' .
+                                strtoupper($card->district_code) . '
+                                ' .
+                                $card->control_number }}</p>
+                            <h5 class=" text-end text-white m-0 ">{{ Auth::user()->name }}</h5>
+                            <p class=" text-end text-white m-0" style="font-size: 10px">{{
+                                Auth::user()->position
+                                }}
+                            </p>
+                            <p class=" text-end text-white m-0" style="font-size: 9px">{{ date('F Y',
+                                strtotime($card->valid_until));
+                                }}
+                            </p>
+                        </div>
+                        <div id="qrcode" class=" float-end ms-2  "></div>
+
+                    </div>
+                </div>
+                <div class="card__face card__face--back"
+                    style="background-image: url('{{ asset('admin/img/BACK_OPTION 1.webp') }}')"></div>
+            </div>
+        </div>
+        {{-- <div class="flip me-2">
             <div class="front w-100 flex"
                 style="background-image: url('{{ asset('admin/img/FRONT_OPTION 1.webp') }}');">
                 <div class=" d-flex align-items-end justify-content-end h-100 w-100 ">
@@ -168,7 +172,7 @@
             </div>
             <div class="back" style="background-image: url('{{ asset('admin/img/BACK_OPTION 1.webp') }}')">
             </div>
-        </div>
+        </div> --}}
         @else <div class="alert alert-danger">
             <p>Your account is not yet approved. Please wait for the admin to approve your account.</p>
         </div>
